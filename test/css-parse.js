9c785afafd489042cedc858acffd289f455b7434
TJ Holowaychuk <tj@vision-media.ca>
2012-07-18 15:59:33 -0700

    Initial commit

---
+++ test/css-parse.js
@@ -0,0 +1,24 @@
+
+/**
+ * Module dependencies.
+ */
+
+var parse = require('..')
+  , fs = require('fs')
+  , path = require('path')
+  , read = fs.readFileSync
+  , readdir = fs.readdirSync;
+
+describe('parse(str)', function(){
+  readdir('test/cases').forEach(function(file){
+    if (~file.indexOf('json')) return;
+    file = path.basename(file, '.css');
+    it('should parse ' + file, function(){
+      var css = read(path.join('test', 'cases', file + '.css'), 'utf8');
+      var json = read(path.join('test', 'cases', file + '.json'), 'utf8');
+      var ret = parse(css);
+      ret = JSON.stringify(ret, null, 2);
+      ret.should.equal(json);
+    })
+  });
+})
